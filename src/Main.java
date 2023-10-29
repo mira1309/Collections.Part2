b0ff02f1b56e7463fb6de33909707fb8e88134b2
Ильмира <kushkarova.1998@mail.ru>
2023-10-29 12:12:35 +0300

    initial code

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
+    }
+}
